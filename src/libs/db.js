565dd3eb6a2916151b358afa79ccd663858e8110
topether21 <topether21@gmail.com>
2023-05-19 09:50:15 -0600

    feat: add typescript

---
+++ src/libs/db.js
@@ -0,0 +1,208 @@
+const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+
+const client = DynamoDBDocument.from(new DynamoDB(), {
+  marshallOptions: { removeUndefinedValues: true, convertEmptyValues: true },
+});
+
+function removeUndefinedValues(obj) {
+  for (let k in obj) {
+    if (obj[k] === undefined) {
+      delete obj[k];
+    } else if (typeof obj[k] === "object" && obj[k] !== null) {
+      removeUndefinedValues(obj[k]);
+    }
+  }
+}
+
+const getAuctionsByNostrAddress = async (nostrAddress) => {
+  console.log(`nostrAddress: ${nostrAddress}, type: ${typeof nostrAddress}`);
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    IndexName: "nostrAddress-index",
+    KeyConditionExpression: "#na = :na",
+    ExpressionAttributeNames: {
+      "#na": "nostrAddress",
+    },
+    ExpressionAttributeValues: {
+      ":na": nostrAddress,
+    },
+  };
+
+  try {
+    const { Items } = await client.query(params);
+    return Items;
+  } catch (error) {
+    console.error(`Error getting auctions by nostrAddress: ${error}`);
+    throw error;
+  }
+};
+
+const getAuctionsByInscriptionId = async (inscriptionId) => {
+  console.log(`inscriptionId: ${inscriptionId}, type: ${typeof nostrAddress}`);
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    IndexName: "inscriptionId-index",
+    KeyConditionExpression: "#na = :na",
+    ExpressionAttributeNames: {
+      "#na": "inscriptionId",
+    },
+    ExpressionAttributeValues: {
+      ":na": inscriptionId,
+    },
+  };
+
+  try {
+    const { Items } = await client.query(params);
+    return Items;
+  } catch (error) {
+    console.error(`Error getting auctions by nostrAddress: ${error}`);
+    throw error;
+  }
+};
+
+const saveAuction = async (auction) => {
+  removeUndefinedValues(auction);
+  if (typeof auction.nostrAddress !== "string") {
+    throw new Error("nostrAddress must be a string");
+  }
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: auction,
+  };
+
+  try {
+    await client.put(params);
+  } catch (error) {
+    console.error(`Error saving auction: ${error}`);
+    throw error;
+  }
+};
+
+const listAuctions = async () => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+  };
+
+  try {
+    const { Items } = await client.scan(params);
+    return Items;
+  } catch (error) {
+    console.error(`Error listing auctions: ${error}`);
+    throw error;
+  }
+};
+
+const getAuction = async (auctionId) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: auctionId,
+    },
+  };
+
+  try {
+    const { Item } = await client.get(params);
+    return Item;
+  } catch (error) {
+    console.error(`Error getting auction: ${error}`);
+    throw error;
+  }
+};
+
+const finishAuction = async (auctionId) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: { id: auctionId },
+    UpdateExpression: "set #status = :status",
+    ExpressionAttributeValues: {
+      ":status": "FINISHED",
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
+
+  try {
+    await client.update(params);
+    return { status: "FINISHED" };
+  } catch (error) {
+    console.error(`Error finishing auction: ${error}`);
+    throw error;
+  }
+};
+
+const updateAuctionStatus = async (auctionId, status) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: auctionId,
+    },
+    UpdateExpression: "SET #status = :status",
+    ExpressionAttributeValues: {
+      ":status": status,
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
+
+  try {
+    await client.update(params);
+  } catch (error) {
+    console.error(`Error updating auction status: ${error}`);
+    throw error;
+  }
+};
+
+const updateAuctionMetadata = async (auctionId, metadata) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: auctionId,
+    },
+    UpdateExpression: "SET metadata = :metadata",
+    ExpressionAttributeValues: {
+      ":metadata": metadata,
+    },
+  };
+
+  try {
+    await client.update(params);
+  } catch (error) {
+    console.error(`Error updating auction status: ${error}`);
+    throw error;
+  }
+};
+
+const updateAuctionPrice = async (auctionId, price) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: auctionId,
+    },
+    UpdateExpression: "SET currentPrice = :currentPrice",
+    ExpressionAttributeValues: {
+      ":currentPrice": price,
+    },
+  };
+
+  try {
+    await client.update(params);
+  } catch (error) {
+    console.error(`Error updating auction price: ${error}`);
+    throw error;
+  }
+};
+
+module.exports = {
+  saveAuction,
+  getAuction,
+  updateAuctionStatus,
+  updateAuctionMetadata,
+  updateAuctionPrice,
+  getAuctionsByNostrAddress,
+  getAuctionsByInscriptionId,
+  listAuctions,
+  finishAuction,
+};
---
+++ src/libs/inscriptions.js
@@ -0,0 +1,53 @@
+const axios = require("./axios");
+
+const TESTNET = false;
+const MEMPOOL_API_URL = TESTNET
+  ? "https://mempool.space/testnet"
+  : "https://mempool.deezy.io";
+
+const config = { MEMPOOL_API_URL };
+
+// https://mempool.deezy.io/api/tx/29a3e9a9d494e01c07e67e9cf9e33b96d4d4b404bab5293ea25b3ffd10d7d46d/outspend/0
+// https://mempool.deezy.io/api/blocks/tip/height
+// {
+//   "spent": true,
+//   "txid": "e14c4c6e881288b7a82d6e610a058ae48db7a1e23cfcbbd8b2af36fc61471a22",
+//   "vin": 1,
+//   "status": {
+//     "confirmed": true,
+//     "block_height": 789711,
+//     "block_hash": "000000000000000000026ef7b109c77b0e88b7ed7dee0eb2ba9f894d5defc36e",
+//     "block_time": 1684075309
+//   },
+//   "confirmations": 371
+// }
+// https://mempool.deezy.io/api/tx/4fc73642d443d86595a693c521aeb6316d8e018411109c6caf8aa60dd9ea97d0/outspend/0
+// {
+//   "spent": false
+// }
+
+const isSpent = async (output) => {
+  //   return {
+  //     spent: false,
+  //   };
+  const [txid, vout] = output.split(":");
+  const { data } = await axios.get(
+    `${config.MEMPOOL_API_URL}/api/tx/${txid}/outspend/${vout}`
+  );
+  return data;
+  // if (data.spent) {
+  //   const { data: last_lock_height } = await axios.get(
+  //     `${config.MEMPOOL_API_URL}/api/blocks/tip/height`
+  //   );
+  //   const confirmations = last_lock_height - data.status.block_height;
+  //   return {
+  //     ...data,
+  //     confirmations,
+  //   };
+  // }
+  // return data;
+};
+
+module.exports = {
+  isSpent,
+};
